c6c3680aeb6a3fc610f244c384a32b14fc0bf866
Sahithi-devAI <sahithichittineni@gmail.com>
2026-03-18 23:46:58 +0530

    Update db_to_json.py

--- db_to_json.py
+++ db_to_json.py
@@ -3,8 +3,8 @@ import os
 
 db_path = os.path.join("database", "lenny_insights.db")
 
-conn = sqlite3.connect(db_path)
-conn.row_factory = sqlite3.Row
+# conn = sqlite3.connect(db_path)
+# conn.row_factory = sqlite3.Row
 cursor = conn.cursor()
 
 mechanisms = cursor.execute("""
